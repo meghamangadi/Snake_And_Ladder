55efcfce9ffb57beb7d71355d8c06ac25143191d
megha angadi <meghamangadi@gmail.com>
2021-08-26 22:57:06 +0530

    Added: math.random function to generate number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,15 +1,21 @@
 package com.bridgeLabz;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
 
 	final static int NO_OF_PLAYER = 1;
+	final static int START_POSITION = 0;
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Snake And Ladder Game");
 		int position = 0;
+
+		int diceValue = (int) Math.floor(Math.random() * 6) + 1;
 		System.out.println("Player start position : " + position);
 		System.out.println("Number of player is:" + NO_OF_PLAYER);
+		System.out.println("Player rolls the dice and get :" + diceValue);
 
 	}
 
