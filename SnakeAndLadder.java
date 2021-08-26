dafb0b49cf296f4cbdb265b183acd518d3516f38
megha angadi <meghamangadi@gmail.com>
2021-08-26 23:05:01 +0530

    Added: More Options_Either Player will get Snake Ladder Or NoPlay

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,21 +1,39 @@
 package com.bridgeLabz;
 
-import java.util.Scanner;
+import java.lang.*;
 
 public class SnakeAndLadder {
 
-	final static int NO_OF_PLAYER = 1;
-	final static int START_POSITION = 0;
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
 
 	public static void main(String[] args) {
-
-		System.out.println("Welcome To Snake And Ladder Game");
-		int position = 0;
+		int start = 0;
+		int roll = 0;
 
 		int diceValue = (int) Math.floor(Math.random() * 6) + 1;
-		System.out.println("Player start position : " + position);
-		System.out.println("Number of player is:" + NO_OF_PLAYER);
-		System.out.println("Player rolls the dice and get :" + diceValue);
+		System.out.println("Dice value is :" + diceValue);
+
+		int option = (int) Math.floor(Math.random() * 10) % 3;
+		System.out.println("Option is :" + option);
+
+		switch (option) {
+		case NO_PLAY:
+			roll = roll;
+			break;
+		case LADDER:
+			roll = +diceValue;
+			break;
+		case SNAKE:
+			roll -= diceValue;
+			if (roll < 0) {
+				roll = 0;
+			}
+			break;
+		}
+		int position = (start + roll);
+		System.out.println("Current Position:" + position);
 
 	}
 
