096cfd69822c5deaa5deaed762b0801e809a2edd
megha angadi <meghamangadi@gmail.com>
2021-08-26 23:10:21 +0530

    Added: Repeat till the Player reaches the wining position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,34 +7,45 @@ public class SnakeAndLadder {
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
+	public static final int WINNING_POSITION = 100;
 
 	public static void main(String[] args) {
-		int start = 0;
-		int roll = 0;
+		int position = 0;
+		int reach = 0;
+		while (reach < WINNING_POSITION) {
+			reach++;
 
-		int diceValue = (int) Math.floor(Math.random() * 6) + 1;
-		System.out.println("Dice value is :" + diceValue);
+			int diceValue = (int) Math.floor(Math.random() * 6) + 1;
+			System.out.println("Dice value is :" + diceValue);
 
-		int option = (int) Math.floor(Math.random() * 10) % 3;
-		System.out.println("Option is :" + option);
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("Option is :" + option);
 
-		switch (option) {
-		case NO_PLAY:
-			roll = roll;
-			break;
-		case LADDER:
-			roll = +diceValue;
-			break;
-		case SNAKE:
-			roll -= diceValue;
-			if (roll < 0) {
-				roll = 0;
+			switch (option) {
+			case NO_PLAY:
+				position += 0;
+				break;
+			case LADDER:
+				if ((position + diceValue) < 100) {
+					position += diceValue;
+					reach = position;
+				} else
+					position += diceValue;
+				reach = position;
+
+				break;
+			case SNAKE:
+				if (position > 0) {
+					position -= diceValue;
+					reach = position;
+				} else
+					position = 0;
+				reach = position;
+
+				break;
 			}
-			break;
+			System.out.println("Reached:" + reach);
 		}
-		int position = (start + roll);
-		System.out.println("Current Position:" + position);
-
+		System.out.println("Win:" + reach);
 	}
-
 }
