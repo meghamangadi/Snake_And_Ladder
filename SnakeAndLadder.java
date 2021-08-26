da116a7bb2bd03eaa0f7c9e5f9464359981d6714
megha angadi <meghamangadi@gmail.com>
2021-08-26 22:47:43 +0530

    Initializing starting Position at Zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,12 +2,15 @@ package com.bridgeLabz;
 
 public class SnakeAndLadder {
 
-	
+	final static int NO_OF_PLAYER = 1;
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Snake And Ladder Game");
-		
+		int position = 0;
+		System.out.println("Player start position : " + position);
+		System.out.println("Number of player is:" + NO_OF_PLAYER);
+
 	}
 
 }
